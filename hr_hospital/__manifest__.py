bda3effa3427382785a020877f5a9ec895071cea
volodymyr <rostotskyi.edu@gmail.com>
2025-03-21 16:04:24 +0000

    Add module metadata and license file

--- hr_hospital/__manifest__.py
+++ hr_hospital/__manifest__.py
@@ -1,1 +1,29 @@
-{}
+{
+    'name': 'Hospital Management',
+    'summary': '',
+    'author': 'Odoo School',
+    'website': 'https://odoo.school/',
+    'category': 'Customizations',
+    'license': 'OPL-1',
+    'version': '17.0.1.0.0',
+
+    'depends': [
+        'base',
+    ],
+
+    'external_dependencies': {
+        'python': [],
+    },
+
+    'data': [
+    ],
+     'demo': [
+    ],
+
+    'installable': True,
+    'auto_install': False,
+
+    'images': [
+    ],
+
+}
